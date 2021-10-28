d1ef602f238874f63991cb13fcc350438b12a0bb
julietamorrazo <92743415+julietamorrazo@users.noreply.github.com>
2021-10-28 08:32:43 -0300

    Delete ej2a.js

--- ej2a.js
+++
@@ -1,7 +0,0 @@
-//2) Escribir una función que tome por parámetro una lista de strings eliminar la palabra mas corta ("hola".length => 4)
-
-let lista = [ "telefono", "soga", "tobogan", "castillo"];
-
-function eliminarPalabra {let a =  (lista.length x=> 6 = )
-
-  
